599bd27154a8a18a118070d74a9f52a3aca86297
Gokul Sundeep <150696266+GokulSundeep29@users.noreply.github.com>
2026-04-21 23:02:44 +0530

    added comments

--- app/main.py
+++ app/main.py
@@ -6,10 +6,12 @@ load_dotenv()
 
 app = Flask(__name__)
 
+# return the string in reverse
 @app.route('/<string>')
 def return_backwards_string(string):
     return "".join(reversed(string))
 
+# check the mode from environment variable
 @app.route('/get-mode')
 def get_mode():
     return os.getenv('MODE', 'No mode set')
